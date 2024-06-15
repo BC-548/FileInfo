3fadc23df4359b22f0c12899c25a4d4f02f91574
BC-548 <101848660+BC-548@users.noreply.github.com>
2024-06-15 22:32:05 +1000

    feat: make command-line friendly

--- Fileinfo Lookup.sh
+++ Fileinfo Lookup.sh
@@ -1,21 +1,28 @@
 #!/bin/zsh
 
+ext=$1
 
-while true
-do
-
-echo -n "File Extension: " 
-read ext
-
-if [[ -z "${ext// }" ]]; then # if there is no input
-    echo "No extension entered! Please enter a file extension"
-    
-else
+openURL() {
     if [[ $ext == .* ]]; then # check if $ext starts with a dot
-        ext="${ext:1}"
+        ext="${ext:1}"        # remove that dot (first character)
     fi
     echo "https://fileinfo.com/extension/$ext"
     open "https://fileinfo.com/extension/$ext" # open FileInfo page with specified extension
-fi
+}
 
-done
+case $ext in
+    "") 
+        # no input
+        echo "Interactive mode"
+        while true; do
+            echo -n "File Extension: "
+            read ext
+            openURL
+        done
+        ;;
+
+    *)
+        openURL
+        ;;
+
+esac
