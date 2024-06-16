93da51a97daa65678c2c7b0f37363af3ae9b10e2
BC-548 <101848660+BC-548@users.noreply.github.com>
2024-06-16 12:08:24 +1000

    feat: add arguments for Python version

--- Fileinfo Lookup.py
+++ Fileinfo Lookup.py
@@ -1,15 +1,26 @@
+import webbrowser
+import sys
+
 browserPath = 'open -a /Applications/Google\\ Chrome.app %s' # change to the path of your preferred web browser
 
 
-import webbrowser
-while True:
-    ext = input('File Extension: ')
-    
-    if not ext or ext.isspace() : # check if the input contains nothing
-        print("No extension entered! Please enter a filename extension")
-    
-    else:
-        if ext.startswith("."):
-            ext = ext[1:] # remove the '.' in front of the file extension
-        print('https://fileinfo.com/extension/' + ext)
-        webbrowser.get(browserPath).open('https://fileinfo.com/extension/' + ext) # Open FileInfo page with specified extension
+def openURL():
+    global ext
+    if ext.startswith("."):
+        ext = ext[1:] # remove the '.' in front of the file extension
+    print('https://fileinfo.com/extension/' + ext)
+    webbrowser.get(browserPath).open('https://fileinfo.com/extension/' + ext) # Open FileInfo page with specified extension
+
+
+if len(sys.argv) == 1: # if there is no arguments passed to the script
+    print("Interactive mode")
+    try:
+        while True:
+            ext = input('File Extension: ')
+            openURL()
+    except KeyboardInterrupt: # do not throw an error on ctrl-c
+        sys.exit()
+
+else:
+    ext = sys.argv[1]
+    openURL()
