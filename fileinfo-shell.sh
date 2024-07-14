54c86d1fff3263efbda2484cb423d29f1b8f9dfc
BC-548 <101848660+BC-548@users.noreply.github.com>
2024-07-14 10:46:42 +1000

    fix: add error message

--- fileinfo-shell.sh
+++ fileinfo-shell.sh
@@ -6,7 +6,7 @@ openURL() {
     if [[ $ext == .* ]]; then # check if $ext starts with a dot
         ext="${ext:1}"        # remove that dot (first character)
     fi
-    echo "https://fileinfo.com/extension/$ext"
+    echo "https://fileinfo.com/extension/$ext\n"
     open "https://fileinfo.com/extension/$ext" # open FileInfo page with specified extension
 }
 
@@ -17,6 +17,10 @@ case $ext in
         while true; do
             echo -n "File Extension: "
             read ext
+            if [ -z $ext ]; then
+                echo "\nPlease enter a file extension"
+                continue
+            fi
             openURL
         done
         ;;
